47ea8ebc128f8312a366a1dd13bf8eeab9438748
Amirul <amirul@innoractive.com>
2025-11-05 12:52:53 +0800

    fix: remove dependency on deprecated Fresco ImageSource for RN 0.75+

--- android/src/main/java/com/dylanvann/fastimage/FastImageSource.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageSource.java
@@ -1,39 +1,57 @@
 package com.dylanvann.fastimage;
 
 import android.net.Uri;
+import android.text.TextUtils;
+
 import androidx.annotation.Nullable;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.modules.fresco.ImageSource;
 
-public class FastImageSource {
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
+import com.facebook.react.bridge.ReadableMap;
 
-    private final ImageSource imageSource;
+class FastImageSource {
+    private final Uri uri;
+    private final GlideUrl glideUrl;
 
-    public FastImageSource(ReactContext reactContext, String source, @Nullable double width, @Nullable double height) {
-        this.imageSource = new ImageSource(reactContext, source, width, height);
-    }
+    FastImageSource(@Nullable ReadableMap source) {
+        if (source != null && source.hasKey("uri")) {
+            String uriStr = source.getString("uri");
+            if (uriStr != null && !uriStr.isEmpty()) {
+                uri = Uri.parse(uriStr);
+            } else {
+                uri = Uri.EMPTY;
+            }
 
-    public FastImageSource(ReactContext reactContext, String source) {
-        this.imageSource = new ImageSource(reactContext, source);
+            if (source.hasKey("headers")) {
+                ReadableMap headers = source.getMap("headers");
+                LazyHeaders.Builder builder = new LazyHeaders.Builder();
+                if (headers != null) {
+                    for (String key : headers.toHashMap().keySet()) {
+                        String value = headers.getString(key);
+                        if (!TextUtils.isEmpty(key) && value != null) {
+                            builder.addHeader(key, value);
+                        }
+                    }
+                }
+                glideUrl = new GlideUrl(uri.toString(), builder.build());
+            } else {
+                glideUrl = new GlideUrl(uri.toString());
+            }
+        } else {
+            uri = Uri.EMPTY;
+            glideUrl = null;
+        }
     }
 
     public Uri getUri() {
-        return imageSource.getUri();
+        return uri;
     }
 
-    public boolean isResource() {
-        return imageSource.isResource();
+    public GlideUrl getGlideUrl() {
+        return glideUrl;
     }
 
-    public String getSource() {
-        return imageSource.getSource();
-    }
-
-    public double getSize() {
-        return imageSource.getSize();
-    }
-
-    public ImageSource getImageSource() {
-        return imageSource;
+    public Object getSourceForLoad() {
+        return glideUrl != null ? glideUrl : uri;
     }
 }
